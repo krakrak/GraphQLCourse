dc798e9b81d3f06747ca0c454437eab779f00562
krakrak <jean-c-irak@hotmail.fr>
2018-03-27 23:18:27 +0200

    CRUD with GraphQL

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,7 +1,14 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import {ApolloProvider}  from 'react-apollo';
+
+import App from './component/App';
+import SongList from './component/SongList';
+import SongCreate from './component/SongCreate';
+import SongDetail from './component/SongDetail';
 
 const client = new ApolloClient({});
 
@@ -9,7 +16,13 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
